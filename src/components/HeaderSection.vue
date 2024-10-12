e7c9cc87ca7533fde38038e91a77b0f90830c742
Yihong Wang <bellowswang@gmail.com>
2024-10-12 20:26:44 +0200

    [TASK1-78] Header adjustment

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="header">
-        <div class="left">
-            <h1 @click="navigate('works')" class="clickable-title">Daftpeng/peng</h1>
-        </div>
-        <div class="right">
-            <a @click="navigate('works')">Works</a>
-            <a @click="navigate('about')">About</a>
+        <div class="header-content">
+            <div class="left">
+                <h1 @click="navigate('works')" class="clickable-title">Daftpeng/peng</h1>
+            </div>
+            <div class="right">
+                <a @click="navigate('works')">Works</a>
+                <a @click="navigate('about')">About</a>
+            </div>
         </div>
     </div>
 </template>
@@ -29,15 +31,25 @@ export default defineComponent({
     position: fixed;
     top: 0;
     padding: 20px;
-    width: 50%;
+    width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
     background-color: #ffffff;
     z-index: 1000;
-    transform: translateX(50%);
     /* Ensure the header stays on top */
     /* Optional shadow for a floating effect */
+}
+
+
+.header-content {
+    width: 50%;
+    /* Restrict the content width to 50% */
+    display: flex;
+    justify-content: space-between;
+    /* Ensure elements within the content are spaced evenly */
+    align-items: center;
+    transform: translateX(50%);
 }
 
 .left h1 {
